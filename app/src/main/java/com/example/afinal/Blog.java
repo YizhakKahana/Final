0a346e8f9259ebb03f8e4399a1d961b1144c415e
Itzik Kahana <ikahana@allot.com>
2019-02-26 18:34:34 +0200

    Add Blog class and layout include ImageView and TextView for title and for description

---
+++ app/src/main/java/com/example/afinal/Blog.java
@@ -0,0 +1,42 @@
+package com.example.afinal;
+
+
+public class Blog {
+
+    private String title;
+    private String description;
+    private String image;
+
+    public Blog() {
+    }
+
+    public Blog(String title, String description, String image) {
+        this.title = title;
+        this.description = description;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String desc) {
+        this.description = desc;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
